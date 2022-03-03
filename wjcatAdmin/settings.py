821e2e2928fe31e9f6a323e8e394632f001f6450
yanqing.wang <615970781@qq.com>
2022-03-03 21:24:28 +0800

    test7

--- wjcatAdmin/settings.py
+++ wjcatAdmin/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': ['myAdmin/dist']
-        'DIRS': ['static/dist/index.html']
+        'DIRS': ['static/dist']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
